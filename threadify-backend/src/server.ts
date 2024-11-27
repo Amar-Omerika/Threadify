2cc8dfca1e9b92b59b34a065832558c2457b9265
Amar-Omerika <amar.omerika@edu.fit.ba>
2024-11-27 16:20:03 +0100

    feat: add cors

--- threadify-backend/src/server.ts
+++ threadify-backend/src/server.ts
@@ -5,11 +5,19 @@ import userRoutes from '@routes/userRoutes';
 import topicRoutes from '@routes/topicRoutes';
 import likeRoutes from '@routes/likeRoutes';
 import commentRoutes from '@routes/commentRoutes';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+    optionsSuccessStatus: 200,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
